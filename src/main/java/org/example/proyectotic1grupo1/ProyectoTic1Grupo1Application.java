d501f91a5de21c804cb86342cc55d5de0adcf49c
PipeG09 <103771722+PipeG09@users.noreply.github.com>
2024-08-22 12:23:08 -0300

    Start

---
+++ src/main/java/org/example/proyectotic1grupo1/ProyectoTic1Grupo1Application.java
@@ -0,0 +1,13 @@
+package org.example.proyectotic1grupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTic1Grupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTic1Grupo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/proyectotic1grupo1/ProyectoTic1Grupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.proyectotic1grupo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTic1Grupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
