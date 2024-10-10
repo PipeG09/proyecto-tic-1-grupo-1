6d46080a68acf63282b5975907eb795e05644d39
SDeGiuda <santiagodegiuda@gmail.com>
2024-10-10 12:20:23 -0300

    ,'+`

--- src/main/java/org/example/proyectotic1grupo1/ProyectoTic1Grupo1Application.java
+++ src/main/java/org/example/proyectotic1grupo1/ProyectoTic1Grupo1Application.java
@@ -1,5 +1,5 @@
 package org.example.proyectotic1grupo1;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoTic1Grupo1Application {
 
 	public static void main(String[] args) {
+		// Carga las variables del archivo .env
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// Establece las variables de entorno para que Spring Boot las reconozca
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(ProyectoTic1Grupo1Application.class, args);
 	}
 
