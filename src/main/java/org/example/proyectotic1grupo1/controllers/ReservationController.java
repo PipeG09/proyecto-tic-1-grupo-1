38b807955d40c23cf727679261f4b1d94f18dc0e
SDeGiuda <santiagodegiuda@gmail.com>
2024-11-02 17:17:28 -0300

    it seems to be working

--- src/main/java/org/example/proyectotic1grupo1/controllers/ReservationController.java
+++ src/main/java/org/example/proyectotic1grupo1/controllers/ReservationController.java
@@ -4,7 +4,9 @@ import jdk.jfr.Percentage;
 import org.example.proyectotic1grupo1.models.Reservation;
 import org.example.proyectotic1grupo1.models.User;
 import org.example.proyectotic1grupo1.services.ReservationService;
+import org.example.proyectotic1grupo1.services.UserSessionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
@@ -18,7 +20,8 @@ public class ReservationController {
 
 
     private final ReservationService reservationService;
-
+    @Autowired
+    UserSessionService userSessionService;
     @Autowired
     public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
@@ -26,7 +29,16 @@ public class ReservationController {
 
 
     @PostMapping("/{screening}")
-    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
+    public ResponseEntity<?> createReservation(@RequestBody Reservation reservation) {
+        if (!userSessionService.loggedIn()){// check user is logged in
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+
+
+        if(reservation.getUserId()!=(userSessionService.getCurrentUser().getId())){ // user can only delete his own reservations
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+
         int savedReservation = reservationService.reserveSeats(reservation);
         if (savedReservation == 1) {
             return ResponseEntity.ok(reservation);
@@ -36,15 +48,26 @@ public class ReservationController {
 
     // Endpoint para obtener todas las reservas
     @GetMapping("/reservations")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<List<Reservation>> getAllReservations(Model model) {
-        User user = (User) model.getAttribute("user");
+    public ResponseEntity<?> getAllReservations() {
+        if (!userSessionService.loggedIn()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+        User user = userSessionService.getCurrentUser();
         List<Reservation> reservations = reservationService.findAll(user);
         return ResponseEntity.ok(reservations);
     }
 
     @DeleteMapping("/cancel/{reservation_id}")
     public ResponseEntity<?> cancelReservation(@PathVariable int reservation_id) {
+        Reservation reservation = reservationService.findById(reservation_id);
+
+        if (!userSessionService.loggedIn()){// check user is logged in
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+        if(reservation.getUserId()!=(userSessionService.getCurrentUser().getId())){ // user can only delete his own reservations
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+
         int  cancelation = reservationService.cancelReservedSeats(reservation_id);
         if (cancelation == 1) {
             return ResponseEntity.ok().build();
